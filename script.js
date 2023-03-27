b574bbf15325c89ed29a2c5fef34f00d10f20165
samruddhi kunale <kunaleashwini3@gmail.com>
2023-03-27 11:55:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+let buttone = document.querySelectorAll('.button-e');
+let buttonc = document.querySelectorAll('.button-c');
+Array.from(buttons).forEach((buttons) => {
+    buttons.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'C') {
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == "//") {
+            string = " error"
+            document.querySelector('input').value = string;
+
+        }
+        else {
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+
+    })
+})
+
+
+
+
